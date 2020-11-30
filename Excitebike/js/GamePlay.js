76458f716ff33f4b754491543c93170dedd29b38
Victor Blas <victor.blas@enti.cat>
2020-11-30 16:43:13 +0100

    Animations created

--- Excitebike/js/GamePlay.js
+++ Excitebike/js/GamePlay.js
@@ -9,13 +9,13 @@ class GamePlay extends Phaser.Scene{
         ruta += 'pilot/';
         this.load.image('motorbike',ruta + 'motorbike.png');
         this.load.spritesheet('pilotLoop', ruta + 'pilot_loop.png', {frameWidth: 98/4, frameHeight: 24});
-        this.load.spritesheet('pilotFrontTilt', ruta + 'pilot_front_tilt.png', {frameWidth: 92/4, frameHeight: 25});
-        this.load.spritesheet('pilotWheelies', ruta + 'pilot_wheelies.png', {frameWidth: 138/6, frameHeight: 26});
-        this.load.spritesheet('pilotGetUp', ruta + 'pilot_get_up', {frameWidth: 77/3, frameHeight: 21});
+        this.load.spritesheet('pilotFrontTilt', ruta + 'pilot_front_tilt.png', {frameWidth: 92/4, frameHeight: 25}); //sa de fer depenent de input, no amb animacio
+        this.load.spritesheet('pilotWheelies', ruta + 'pilot_wheelies.png', {frameWidth: 138/6, frameHeight: 26});//sa de fer depenent de input, no amb animacio
+        this.load.spritesheet('pilotGetUp', ruta + 'pilot_get_up.png', {frameWidth: 77/3, frameHeight: 21});
         this.load.spritesheet('pilotMoving', ruta + 'pilot_moving.png',{frameWidth: 40/2, frameHeight: 21});
-        this.load.spritesheet('pilotRunning', ruta + 'pilot_running',{frameWidth: 28/2, frameHeight: 16});
-        this.load.image('pilotTurnLeft',ruta + 'pilot_turning_left');
-        this.load.image('pilotTurnRight',ruta + 'pilot_turning_right');
+        this.load.spritesheet('pilotRunning', ruta + 'pilot_running.png',{frameWidth: 28/2, frameHeight: 16});
+        this.load.image('pilotTurnLeft',ruta + 'pilot_turning_left.png');
+        this.load.image('pilotTurnRight',ruta + 'pilot_turning_right.png');
 
     }
     create(){
@@ -25,16 +25,35 @@ class GamePlay extends Phaser.Scene{
         this.anims.create({
             key: 'running',
             frames: this.anims.generateFrameNumbers('pilotRunning', { start: 0, end: 1 }),
-            frameRate: 10,
+            frameRate: 5,
             repeat: -1
         });
-
+        this.anims.create({
+            key: 'moving',
+            frames: this.anims.generateFrameNumbers('pilotMoving', { start: 0, end: 1 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'loop',
+            frames: this.anims.generateFrameNumbers('pilotLoop', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'getUp',
+            frames: this.anims.generateFrameNumbers('pilotGetUp', { start: 0, end: 2 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.pilot.anims.play('moving', true);
         this.inputs = new InputManager(this);
+
     }
     update(){
-        if(this.inputs.A_Key.isDown){
-            this.pilot.anims.play('running', true);
-        }
+        
+            
+         
     }
 }
 
