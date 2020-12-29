355bf3384db663c77b602ff13ba29f8d7c37a722
Francesc Obrador <francesc.obrador@enti.cat>
2020-12-29 15:32:26 +0100

    Need Timer Branch

--- Excitebike/js/EndgameMenu.js
+++ Excitebike/js/EndgameMenu.js
@@ -1,6 +1,4 @@
 class EndgameMenu extends Phaser.Scene{
-	
-
 	constructor(){
 		 super({key: 'EndgameMenu'});
 	}
@@ -9,12 +7,12 @@ class EndgameMenu extends Phaser.Scene{
 		var ruta = "assets/img/Ranking/";
 		this.load.image('screen', ruta + "RankingScreen.png");
 
-		this.textColor = "#5ce430";
+		this.textColor = "0x5ce430";
 
-		this.bestTimes = ["1:16:00", "1:26:00", "1:36:00"];
-		this.bestTimesInFloat = [75, 86, 96];
+		this.bestTimes = ["1:16:00", "1:26:00", "1:36:00"];	//Excitebike times
+		this.bestTimesInFloat = [76, 86, 96];				// Times equivalent
 
-		this.yourTime = this.bestTimes[0]; // TODO Get the best time from the gameplay scene
+		this.yourTime = 76 // TODO Get the best time from the gameplay scene
 		this.position = "#0";
 
 		if(this.yourTime <= this.bestTimesInFloat[0])
@@ -32,9 +30,9 @@ class EndgameMenu extends Phaser.Scene{
 		else 
 		{
 			// You loose
-			this.position = "#4";
 		}
 
+		// Convert yourtime to string time
 
 		this.load.bitmapFont('nesFont', 'assets/fonts/nes_font_0.png', 'assets/fonts/nes_font.xml');
 
@@ -55,6 +53,7 @@ class EndgameMenu extends Phaser.Scene{
 		else{
 			this.bestTimeText = this.add.bitmapText(config.width/1.45, config.height/1.95, 'nesFont', this.bestTimes[0], 8).setOrigin(0.5); 
 		}
+		this.bestTimeText.tint = this.textColor;
 		
 		this.yourTimeText = this.add.bitmapText(config.width/1.45, config.height/1.7, 'nesFont', this.yourTime, 8).setOrigin(0.5); 
 		this.rankingText = this.add.bitmapText(config.width/1.45, config.height/1.55, 'nesFont', this.position, 8).setOrigin(0.5); 
